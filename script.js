fc3d29f69e500e2ef0b4835332bff8029c88988b
afraz-07 <mohammedafraz0007@gmail.com>
2023-03-11 20:07:39 +0530

    my

--- script.js
+++ script.js
@@ -85,12 +85,18 @@ function playGame(){
         gameOver = true;
     }
 
+    
+
     for(let i = 0; i < snakeBody.length ; i++){
         html += `<div class= "head" style= "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
         if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
             gameOver = true; 
         }
+
+    
     }
+
+    
     gameborad.innerHTML = html;
 }
 changeFood();
